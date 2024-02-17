75231898d071d24ec50d821257d325352db88bd1
Roger Lefort <rogerlefort@Rogers-MacBook-Pro-16.local>
2024-02-16 21:08:09 -0800

    push to streamlit cloud

--- app.py
+++ app.py
@@ -88,22 +88,16 @@ def main():
     st.title('Detecting Pathologies Through Computer Vision in Ultrasound')
     with st.sidebar.title('About'):
         st.markdown("""
-        <h2>Detecting Pathologies Through Computer Vision in Ultrasound</h2>
         <h3>About</h3>
-        <h4>Overview</h4>
         This Streamlit-based web application is designed to assist in the medical analysis by automatically detecting and classifying tumors in ultrasound images as benign or malignant. Utilizing advanced computer vision and machine learning techniques, the app provides healthcare professionals with a rapid, initial assessment tool that aids in the diagnosis and understanding of potential pathologies.
-
-        <h4>Features</h4>
-        <b>Image Upload</b>: Users can upload ultrasound images of tumors for analysis.
-        Contour Analysis: The app processes uploaded images to detect and highlight contours of tumors, facilitating visual assessment of size and shape.
-        Sphericity Measurement: By evaluating the roundness of detected contours, the app assists in identifying characteristics typically associated with benign or malignant tumors.
-        Threshold Adjustment: Users can adjust the threshold settings for contour detection and sphericity to fine-tune the analysis based on specific case needs.
-        Machine Learning Prediction: The core of the app lies in its ability to predict whether a tumor is likely benign or malignant, using a trained machine learning model. The prediction is accompanied by a probability score, providing insight into the model's confidence.
-        <br>
-        <b>Interactive Visuals</b>: The application includes interactive heatmaps and contour overlays, allowing for an in-depth examination of the tumor's properties.e
-        <br>
-        The web application is straightforward to use: simply upload an ultrasound image, and the system will automatically process the image, display relevant visual insights, and provide a prediction for the nature of the tumor.
-        <br>
+        <h3>Features</h3>
+        <li><b>Image Upload</b>: Users can upload ultrasound images of tumors for analysis.<br></li>
+        <li><b>Contour Analysis</b>: The app processes uploaded images to detect and highlight contours of tumors, facilitating visual assessment of size and shape.<br></li>
+        <li><b>Sphericity Measurement</b>: By evaluating the roundness of detected contours, the app assists in identifying characteristics typically associated with benign or malignant tumors.<br></li>
+        <li><b>Threshold Adjustment</b>: Users can adjust the threshold settings for contour detection and sphericity to fine-tune the analysis based on specific case needs.<br></li>
+        <li><b>Deep Learning Prediction</b>: The core of the app lies in its ability to predict whether a tumor is likely benign or malignant, using a trained machine learning model. The prediction is accompanied by a probability score, providing insight into the model's confidence.<br></li>
+        <li><b>Interactive Visuals</b>: The application includes interactive heatmaps and contour overlays, allowing for an in-depth examination of the tumor's properties.<br></li>
+        The web application is straightforward to use: simply upload an ultrasound image, and the system will automatically process the image, display relevant visual insights, and provide a prediction for the nature of the tumor.<br>
         <h4>Technical Details</h4>
         The app employs Convolutional Neural Networks (CNNs) for image analysis and classification, leveraging preprocessed image data to ensure accurate and reliable predictions. Parameters such as contour detection thresholds and sphericity can be adjusted by users to cater to different image qualities and diagnostic requirements.
         <br>
@@ -162,8 +156,7 @@ def main():
                 _heatmap(image, contours)
     except NameError:
         pass
-    
-    
+
     st.markdown("---")
     # Disclaimer
     st.markdown("""
